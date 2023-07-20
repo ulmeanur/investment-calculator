1e8ace7543f4db28fcd9ee1a43423e7d62d964cc
Raluca Ulmeanu <raluca.ioana.ulmeanu@gmail.com>
2023-07-20 23:33:47 +0300

    split the code into 3 main components

--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,30 +1,30 @@
 //Add New Investment
 
-import React from "react";
-import "./ResultsTable.css";
+import React from 'react';
+import './ResultsTable.css';
 
 const ResultsTable = () => {
 	return (
 		<table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+			<thead>
+				<tr>
+					<th>Year</th>
+					<th>Total Savings</th>
+					<th>Interest (Year)</th>
+					<th>Total Interest</th>
+					<th>Invested Capital</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>YEAR NUMBER</td>
+					<td>TOTAL SAVINGS END OF YEAR</td>
+					<td>INTEREST GAINED IN YEAR</td>
+					<td>TOTAL INTEREST GAINED</td>
+					<td>TOTAL INVESTED CAPITAL</td>
+				</tr>
+			</tbody>
+		</table>
 	);
 };
 
--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,42 +1,50 @@
 //Add New Investment
 
-import React from "react";
-import "./UserInput.css";
+import React, { useState } from 'react';
+import './UserInput.css';
 
 const UserInput = () => {
+	const onSubmitHandler = () => {
+		return console.log('Form submitted');
+	};
+
+	const onResetHandler = () => {
+		return console.log('Clear inputs');
+	};
+
 	return (
-        <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
-        </p>
-      </form>
+		<form className="form">
+			<div className="input-group">
+				<p>
+					<label htmlFor="current-savings">Current Savings ($)</label>
+					<input type="number" id="current-savings" />
+				</p>
+				<p>
+					<label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+					<input type="number" id="yearly-contribution" />
+				</p>
+			</div>
+			<div className="input-group">
+				<p>
+					<label htmlFor="expected-return">
+						Expected Interest (%, per year)
+					</label>
+					<input type="number" id="expected-return" />
+				</p>
+				<p>
+					<label htmlFor="duration">Investment Duration (years)</label>
+					<input type="number" id="duration" />
+				</p>
+			</div>
+			<p className="actions">
+				<button type="reset" className="buttonAlt" onClick={onResetHandler}>
+					Reset
+				</button>
+				<button type="submit" className="button" onClick={onSubmitHandler}>
+					Calculate
+				</button>
+			</p>
+		</form>
 	);
 };
 
